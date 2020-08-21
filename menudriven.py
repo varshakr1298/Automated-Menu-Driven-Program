c248cbeb6a1f04d12b2ef4de9363555faf748a41
Varsha KR <43178958+varshakr1298@users.noreply.github.com>
2020-08-21 21:28:41 +0530

    Updated code file

--- menudriven.py
+++ menudriven.py
@@ -5,7 +5,7 @@ p.speak("Can I know your name please")
 name=input("Can I know your name please?: ")
 print()
 ch = 'yes'
-while ch == 'yes':
+while ch=='yes':
     print("***********************************************************************************")
     print("Greetings! Welcome to Automated Menu Driven Program to open applications from MacOS")
     print("***********************************************************************************")
@@ -31,55 +31,55 @@ while ch == 'yes':
     p.speak("Option eight iMovies")
     print("9. Open WhatsApp")
     p.speak("Option nine whatsapp")
-    print("10. Display Calendar ")
-    p.speak("option ten display calendar")
-    print("11. Display today's date")
-    p.speak("option eleven displaying date")
+    print("A. Display Calendar ")
+    p.speak("option a display calendar")
+    print("B. Display today's date")
+    p.speak("option b displaying date")
     p.speak("Choose what you want to do?")
     print()
     choice = input("Choose what you want to do?:").lower()
     
-    if((("open" in choice) and ("google" in choice) and ("chrome" in choice) and ("browser" in choice)) or (int(choice) == 1) or (("open" in choice) and ("chrome" in choice)) or (("launch" in choice) and ("chrome" in choice))):
+    if((("open" in choice) and ("google" in choice) and ("chrome" in choice) and ("browser" in choice)) or ('1' in choice) or (("open" in choice) and ("chrome" in choice)) or (("launch" in choice) and ("chrome" in choice))):
         p.speak("opening chrome browser for you")
         os.system("open -a Chrome.app")
         
-    elif((("open" in choice) and ("calculator" in choice)) or (int(choice) == 2) or (("launch" in choice) and ("calculator" in choice))):
+    elif((("open" in choice) and ("calculator" in choice)) or ('2' in choice) or (("launch" in choice) and ("calculator" in choice))):
         p.speak("opening calculator for you")
         os.system("open -a Calculator.app")
         
-    elif((("open" in choice) and ("camera" in choice)) or (int(choice) == 3) or (("launch" in choice) and ("camera" in choice))):
+    elif((("open" in choice) and ("camera" in choice)) or ('3' in choice) or (("launch" in choice) and ("camera" in choice))):
         p.speak("opening camera for you")
         os.system("open -a FaceTime.app")
 
-    elif((("open" in choice) and ("textedit" in choice)) or (int(choice) == 4) or (("launch" in choice) and ("textedit" in choice))):
+    elif((("open" in choice) and ("textedit" in choice)) or ('4' in choice) or (("launch" in choice) and ("textedit" in choice))):
         p.speak("opening text editor for you")
         os.system("open -a TextEdit.app")
         
-    elif((("open" in choice) and ("siri" in choice) and ("assisstant" in choice)) or (int(choice) == 5) or (("launch" in choice) and ("siri" in choice))):
+    elif((("open" in choice) and ("siri" in choice) and ("assisstant" in choice)) or ('5' in choice) or (("launch" in choice) and ("siri" in choice))):
         p.speak("opening siri assisstant for you")
         os.system("open -a Siri.app")
         
-    elif((("open" in choice) and ("quicktime" in choice) and ("player" in choice)) or (int(choice) == 6) or (("launch" in choice) and ("quicktime" in choice))):
+    elif((("open" in choice) and ("quicktime" in choice) and ("player" in choice)) or ('6' in choice) or (("launch" in choice) and ("quicktime" in choice))):
         p.speak("opening quick time player for you")
         os.system("open -a QuickTime\ Player.app")
         
-    elif((("open" in choice) and ("podcasts" in choice)) or (int(choice) == 7) or (("launch" in choice) and ("podcasts" in choice)) or (("listen" in choice) and ("to" in choice) and ("podcasts"in choice))):
+    elif((("open" in choice) and ("podcasts" in choice)) or ('7' in choice) or (("launch" in choice) and ("podcasts" in choice)) or (("listen" in choice) and ("to" in choice) and ("podcasts"in choice))):
         p.speak("opening podcasts for you")
         os.system("open -a Podcasts.app")
            
-    elif((("open" in choice) and ("imovies" in choice)) or (int(choice) == 8) or (("watch" in choice) and ("imovies" in choice))):
+    elif((("open" in choice) and ("imovies" in choice)) or ('8' in choice) or (("watch" in choice) and ("imovies" in choice))):
         p.speak("opening imovies for you")
         os.system("open -a iMovie.app")
                
-    elif((("open" in choice) and ("whatsapp" in choice)) or (int(choice) == 9) or (("launch" in choice) and ("whatsapp" in choice))):
+    elif((("open" in choice) and ("whatsapp" in choice)) or ('9' in choice) or (("launch" in choice) and ("whatsapp" in choice))):
         p.speak("opening whatsapp for you")
         os.system("open -a WhatsApp.app")
                
-    elif((("display" in choice) and ("calendar" in choice)) or (int(choice) == 10) or (("open" in choice) and ("calendar" in choice))):
+    elif((("display" in choice) and ("calendar" in choice)) or ('a' in choice) or (("open" in choice) and ("calendar" in choice))):
         p.speak("showing calender")
         os.system("cal")
         
-    elif((("display" in choice) and ("date" in choice)) or (int(choice) == 11) or (("show" in choice) and ("date" in choice))):
+    elif((("display" in choice) and ("date" in choice)) or ('b' in choice) or (("show" in choice) and ("date" in choice))):
         p.speak("showing today's date")
         os.system("date")
         
